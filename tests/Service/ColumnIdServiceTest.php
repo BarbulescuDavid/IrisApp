b549267497c5fa257abaede23597db83fed1d4b8
BarbulescuDavid <barbulescudavid1@gmail.com>
2024-05-25 20:35:17 +0300

    Installed mockery

---
+++ tests/Service/ColumnIdServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Service;
+
+use Mockery\MockInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ColumnIdServiceTest extends KernelTestCase
+{
+
+}
